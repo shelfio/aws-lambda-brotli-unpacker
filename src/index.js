4da4fddecce3fa319f70d91b44abf6fa9683ff32
Vlad Holubiev <golubev.vld@gmail.com>
2018-11-03 21:41:44 +0200

    feat: implement unpack function

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const tar = require('tar-fs');
+
+/**
+ * Unpacks brotli archive of a .tar file to /tmp folder
+ * @param {String} inputPath Absolute path to the input brotli archive
+ * @param {String} outputPath Path to the unpacked binary in /tmp folder
+ * @return {Promise<String>} Path to unpacked binary, equals to outputBin
+ * @see https://github.com/alixaxel/chrome-aws-lambda
+ */
+module.exports.unpack = function({inputPath, outputPath}) {
+  return new Promise((resolve, reject) => {
+    let input = path.resolve(inputPath);
+    let output = outputPath;
+
+    if (fs.existsSync(output) === true) {
+      return resolve(output);
+    }
+
+    const source = fs.createReadStream(input);
+    const target = tar.extract('/tmp');
+
+    source.on('error', error => {
+      return reject(error);
+    });
+
+    target.on('error', error => {
+      return reject(error);
+    });
+
+    target.on('finish', () => {
+      fs.chmod(output, '0755', error => {
+        if (error) {
+          return reject(error);
+        }
+
+        return resolve(output);
+      });
+    });
+
+    source.pipe(require(`${__dirname}/iltorb`).decompressStream()).pipe(target);
+  });
+};
